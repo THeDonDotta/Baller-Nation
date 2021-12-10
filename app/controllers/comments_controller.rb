1c327c428abffe8a66fb231132ff1aff8dd132cb
Jeff <jeffjr25@icloud.com>
2021-12-10 13:40:31 -0500

    worked on backend and started setup for front end

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,20 @@
+class CommentsController < ApplicationController
+
+  # GET /comments
+  def index
+    @comments = Comment.all
+
+    render json: @comments
+  end
+  # POST /comments
+  def create
+    @comment = Comment.new(comment_params)
+
+    if @comment.save
+      render json: @comment, status: :created, location: @comment
+    else
+      render json: @comment.errors, status: :unprocessable_entity
+    end
+  end
+
+end
---
+++ db/seeds.rb
@@ -0,0 +1,16 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Post.destroy_all
+Comment.destroy_all
+
+Post.create!(post: 'The 76ers are the best team in the league', image:'https://imgur.com/MOP5vwM')
+
+10.times do 
+    Comment.create!(comment: Faker::Lorem.unique, post: 'The 76ers are the best team in the league')
+end
+
