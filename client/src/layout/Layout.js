1bb868b865e54f44259757310e10e92ccbaca44a
Jeff <jeffjr25@icloud.com>
2021-12-13 13:34:59 -0500

    updated post rendering, standings, tickets

--- client/src/layout/Layout.js
+++ client/src/layout/Layout.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Nav from '../components/nav'
+import Footer from '../components/footer'
+
 function Layout(props) {
     return (
         <div>
             <Nav />
             {props.children}
+            <Footer />
         </div>
     )
 }
