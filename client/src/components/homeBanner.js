929c167e3494039f8bdd223547aa8dd18f18e46b
Jeff <jeffjr25@icloud.com>
2021-12-14 18:51:33 -0500

    added some css

--- client/src/components/homeBanner.js
+++ client/src/components/homeBanner.js
@@ -3,6 +3,9 @@ import '../assets/homeBanner.css'
 function HomeBanner() {
   return (
     <div className= 'homeBanner'>
+
+      <h1 className= 'homeBanner-title'>BALLER-NATION</h1>
+
       <img src="https://i.imgur.com/vA6P1s4.png" title="source: imgur.com" />
     </div>
   )
--- client/src/screens/Standings.js
+++ client/src/screens/Standings.js
@@ -3,6 +3,7 @@ import React from 'react'
 function Standings() {
     return (
         <div>
+            <h1 className="standings-title">Current Standings</h1>
             <img src="https://i.imgur.com/QNgHxlr.png" title="source: imgur.com" />
             <img src="https://i.imgur.com/AfiNiBB.png" title="source: imgur.com" />
         </div>
