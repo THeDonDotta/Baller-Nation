11c6cb577b5a22995784947811e3366f29329df3
Jeff <jeffjr25@icloud.com>
2021-12-15 08:38:48 -0500

    added styling for fonts

--- client/src/screens/About.js
+++ client/src/screens/About.js
@@ -7,11 +7,11 @@ function About() {
             <h1 className="about-title">BALLER-NATION</h1>
             <h4 className="about-description">Where intellegent ballers collaborate</h4>
             <div >
-            <img classname="creative" src="https://i.imgur.com/2qVSzql.png" title="source: imgur.com" />
+            <img classname="creative" src="https://i.imgur.com/2qVSzql.png" />
             <img className="nba-logo" src="https://i.imgur.com/si7hhZc.jpg" title="source: imgur.com" />
             </div>
 
-            <p>Bacon ipsum dolor amet short ribs brisket venison rump drumstick pig sausage prosciutto chicken spare ribs salami picanha doner. Kevin capicola sausage, buffalo bresaola venison turkey shoulder picanha ham pork tri-tip meatball meatloaf ribeye. Doner spare ribs andouille bacon sausage. Ground round jerky brisket pastrami shank.
+            <p className="about-details">Bacon ipsum dolor amet short ribs brisket venison rump drumstick pig sausage prosciutto chicken spare ribs salami picanha doner. Kevin capicola sausage, buffalo bresaola venison turkey shoulder picanha ham pork tri-tip meatball meatloaf ribeye. Doner spare ribs andouille bacon sausage. Ground round jerky brisket pastrami shank.
             </p>
         </div>
     )
