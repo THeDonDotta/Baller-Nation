45c703dacd00cf5e896149fc3c2f30404215d077
Jeff <jeffjr25@icloud.com>
2021-12-15 08:23:42 -0500

    added styling to about

--- client/src/screens/About.js
+++ client/src/screens/About.js
@@ -5,10 +5,10 @@ function About() {
     return (
         <div>
             <h1 className="about-title">BALLER-NATION</h1>
-            <h4>Where intellegent ballers collaborate</h4>
+            <h4 className="about-description">Where intellegent ballers collaborate</h4>
             <div >
-            <img classname="about-images" src="https://i.imgur.com/2qVSzql.png" title="source: imgur.com" />
-            {/* <img classname="about-images" src="https://i.imgur.com/3qvAQBQ.png" title="source: imgur.com" /> */}
+            <img classname="creative" src="https://i.imgur.com/2qVSzql.png" title="source: imgur.com" />
+            <img className="nba-logo" src="https://i.imgur.com/si7hhZc.jpg" title="source: imgur.com" />
             </div>
 
             <p>Bacon ipsum dolor amet short ribs brisket venison rump drumstick pig sausage prosciutto chicken spare ribs salami picanha doner. Kevin capicola sausage, buffalo bresaola venison turkey shoulder picanha ham pork tri-tip meatball meatloaf ribeye. Doner spare ribs andouille bacon sausage. Ground round jerky brisket pastrami shank.
