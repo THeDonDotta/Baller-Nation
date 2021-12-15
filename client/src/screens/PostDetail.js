f5507068ac7da24e677b89d5d4874de50236f7c5
Jeff <jeffjr25@icloud.com>
2021-12-15 13:37:35 -0500

    responsiveness update

--- client/src/screens/PostDetail.js
+++ client/src/screens/PostDetail.js
@@ -74,30 +74,3 @@ function PostDetail({handlePostDelete, handleCommentCreate}) {
 
 export default PostDetail
 
-// {/* <h3>{post?.post}</h3>
-// {post?.comments.map((c) => (
-//     <p key={`list${c.id}`}>{c}</p>
-// ))}
-
-// <form onSubmit={handleSubmit}>
-
-// <select onChange={handleChange} defaultValue='default'>
-// {comments.map((c) => (
-
-//     <option value={c.id} key={`select${c.id}`}>
-//     {c.comment}
-//     </option>
-// ))}
-// </select>
-// <button>Add</button>
-// </form> */}
-
-// const handleChange = (e) => {
-//     const { value } = e.target;
-//     setCommentId(value);
-// };
-
-// const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     const newComments = await addComment({ comment_id: commentId, post_id: id });
-//     setPost((prevState) => ({ ...prevState, comments: newComments }));
